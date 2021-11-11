89fc66e8dea90d7085cb07e689f7a70f3515722a
Katharine-git <katharinesheen@gmail.com>
2021-11-11 11:31:43 +0000

    added

--- IBM_HTTP_SERVER/install_ihs.ps1
+++ IBM_HTTP_SERVER/install_ihs.ps1
@@ -1,25 +1,38 @@
 #downloading package for IBM HTTP SERVER
 
-function IBM_http_server($imcl_path, $master_password_path, $secureStorageFile ){ 
+function IBM_http_server($imcl_path, $package, $ihs_repository, $install_dir, $shared_repo_dir, $port, $creds_path ){ 
 
     #encrypting master password
     #$Secure_String_Pwd = ConvertTo-SecureString $master_password_path -AsPlainText -Force
-
+    Write-Verbose "Installing IBM HTTP SERVER(IHSILAN)"
     Set-Location -Path $imcl_path
-    cmd.exe /c "imcl install com.ibm.websphere.liberty.v85_8.5.16003.20160831_1733 -repositories  http://www.ibm.com/software/repositorymanager/com.ibm.websphere.IHS.v85 -installationDirectory IBM/HTTPServer85 -sharedResourcesDirectory /IBM/UserData/InstallationManager/IMShared -properties user.ihs.httpPort=80 -acceptLicense -showProgress -masterPasswordFile $master_password_path -secureStorageFile $secureStorageFile"
+    cmd.exe /c "imcl install $package -repositories $ihs_repository  -installationDirectory $install_dir -sharedResourcesDirectory $shared_repo_dir -properties user.ihs.httpPort=$port -acceptLicense -showProgress -masterPasswordFile $creds_path\master_password.txt -secureStorageFile $creds_path\secure_storage_file.txt"
   
 
 }
 
 #variables
-$path = "C:\Users\Administrator\Documents\IBM\IBM_HTTP_SERVER\IHS.properties"
+$path = "C:\Users\Administrator\Documents\test\IBM\IBM_HTTP_SERVER\IHS.properties"
 $output = Get-Content $path | ConvertFrom-StringData
 
 $imcl_path=$output.imcl_path
-$master_password_path=$output.master_password_path
-$secureStorageFile=$output.secureStorageFile
-#$Secure_String_Pwd=$output.Secure_String_Pwd
+$package=$output.package
+$ihs_repository=$output.ihs_repository
+$install_dir=$output.install_dir
+$shared_repo_dir=$output.shared_repo_dir
+$port=$output.port
+$creds_path=$output.creds_path
+$VerbosePreference="continue"
 
-IBM_http_server $imcl_path
+
+IBM_http_server $imcl_path $package $ihs_repository $install_dir $shared_repo_dir $port $creds_path
+
+
+if (Test-Path $ihs_install_dir){
+    Write-Verbose "Successfully installed !!"
+  }
+  else{
+    Write-Verbose "Unsuccessfull installment"
+}
 
  
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Powershell script reference link : https://www.powershellgallery.com/packages/cIBMWebSphereAppServer/1.1.2/Content/cIBMWebSphereAppServerUtils.psm1
+Installation of IBM products like IBM Installation manager, IBM Websphere Application and IBM HTTP Server .
+
+Used set of commands necessasry for this process is listed in the commands.txt .
+
+Before installing IBM WAS and IHS m, make sure to create the secure file storage by running storage_file_create.ps1 to avoid frequent prmpt of credentials.
